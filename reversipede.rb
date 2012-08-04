88f4634639c7d3a4415ba28889d10c7de7f3b8cc
Blaine Pace <blainepace@gmail.com>
2012-08-03 19:50:23 -0700

    Rewrote game as class

--- reversipede.rb
+++ reversipede.rb
@@ -3,41 +3,37 @@
 require 'rubygame'
 include Rubygame
 
-screen = Screen.new([400,400], 0)
-screen.title = "Reversipede"
-
-event_queue = EventQueue.new
-event_queue.enable_new_style_events
-
-while(event = event_queue.wait)
-  p event
-  case event
-  when Events::KeyReleased
-    if event.key == :escape
-      break
-    end
-  end
-end
-
 class Game
 
   def initialize
     @screen = Screen.new([400,400], 0)
-    screen.title = "Reversipede"
+    @screen.title = "Reversipede"
     @event_queue = EventQueue.new
     @event_queue.enable_new_style_events
   end
 
+  def start
+    while(!@game_quitting)
+      self.tick
+    end
+  end
+
+  def tick
+    handle_input
+  end
+
+  private
   def handle_input
-    event = event_queue.wait
+    event = @event_queue.wait
     p event
     case event
     when Events::KeyReleased
       if event.key == :escape
-        break
+        @game_quitting = true
       end
     end
   end
-
-
 end
+
+game = Game.new
+game.start
