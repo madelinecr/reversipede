4e92d61bb9852b19d9854d19d00d0b498612c40c
Blaine Pace <blainepace@gmail.com>
2012-08-04 04:06:53 -0700

    Game Loop Cleanup, Refactor, Smooth Movement + FPS
    Cleaned up the main game loop, refactored some methods. Smoothed player movement
    and constrained the game to a constant 30 fps.

--- reversipede.rb
+++ reversipede.rb
@@ -11,38 +11,25 @@ class Game
     @screen.title = "Reversipede"
     @event_queue = EventQueue.new
     @event_queue.enable_new_style_events
+    @clock = Clock.new
+    @clock.target_framerate = 30
     @scale = 16
     @grid_resolution = 20
     @player = Player.new([1, 1], [0xff, 0xff, 0xff])
   end
 
-  def start
+  def run
     create_entities
-    while(!@game_quitting)
-      tick
+    loop do
+      handle_input
+      update
+      draw
+      @screen.update
+      @clock.tick
     end
   end
 
   private
-  def tick
-    handle_input
-    draw
-    @screen.update
-  end
-
-  def handle_input
-    event = @event_queue.wait
-    p event
-    case event
-    when Events::KeyPressed
-      if event.key == :escape
-        @game_quitting = true
-      elsif event.key == :up or :down or :left or :right
-        @player.move event.key
-      end
-    end
-  end
-
   def create_entities
     @tiles = []
     for x in 1..@grid_resolution do
@@ -51,6 +38,30 @@ class Game
       end
     end
     @tiles << @player
+  end
+
+  def handle_input
+    @event_queue.each do |event|
+      case event
+      when Events::KeyPressed
+        if event.key == :escape
+          Rubygame.quit
+          exit
+        elsif event.key == :up or :down or :left or :right
+          @player.on_press event.key
+        end
+      when Events::KeyReleased
+        if event.key == :up or :down or :left or :right
+          @player.on_release event.key
+        end
+      end
+    end
+  end
+
+  def update
+    @tiles.each do |tile|
+      tile.update
+    end
   end
 
   def draw
@@ -78,23 +89,33 @@ class Tile
   def scaled_size(scale)
     pos.collect {|pos| (pos * scale) + scale }
   end
+
+  def update
+  end
 end
 
 class Player < Tile
 
-  def move(args)
-    case args
-    when :up
-      @pos[1] -= 1
-    when :down
-      @pos[1] += 1
-    when :left
-      @pos[0] -= 1
-    when :right
-      @pos[0] += 1
-    end
+  def initialize(pos, color)
+    super
+    @moving = { }
+  end
+
+  def on_press(args)
+    @moving[args] = true
+  end
+
+  def on_release(args)
+    @moving[args] = false
+  end
+
+  def update
+    @pos[1] -= 1 if @moving[:up]
+    @pos[1] += 1 if @moving[:down]
+    @pos[0] -= 1 if @moving[:left]
+    @pos[0] += 1 if @moving[:right]
   end
 end
 
 game = Game.new
-game.start
+game.run
