951188e7a12bde24afc58e2fbd1edd648c7499bb
Blaine Pace <blainepace@gmail.com>
2012-08-03 23:19:37 -0700

    Added player
    Added player class that can be moved around with the arrow keys.

--- reversipede.rb
+++ reversipede.rb
@@ -13,6 +13,7 @@ class Game
     @event_queue.enable_new_style_events
     @scale = 16
     @grid_resolution = 20
+    @player = Player.new([1, 1], [0xff, 0xff, 0xff])
   end
 
   def start
@@ -23,28 +24,9 @@ class Game
   end
 
   private
-  def create_grid
-    @tiles = []
-    for x in 1..@grid_resolution do
-      for y in 1..@grid_resolution do
-        @tiles << Tile.new([x, y], [0x05*x, 0xff, 0x05*y])
-      end
-    end
-    puts @tiles
-  end
-
-  def draw_grid
-    @tiles.each do |tile|
-      tile_position = tile.pos.collect {|pos| pos * @scale }
-      tile_size     = tile.pos.collect {|pos| (pos * @scale) + @scale }
-      @screen.draw_box_s(tile_position, tile_size, tile.color)
-    end
-  end
-
   def tick
     handle_input
-    draw_grid
-    #draw_box
+    draw
     @screen.update
   end
 
@@ -52,11 +34,34 @@ class Game
     event = @event_queue.wait
     p event
     case event
-    when Events::KeyReleased
+    when Events::KeyPressed
       if event.key == :escape
         @game_quitting = true
+      elsif event.key == :up or :down or :left or :right
+        @player.move event.key
       end
     end
+  end
+
+  def create_grid
+    @tiles = []
+    for x in 1..@grid_resolution do
+      for y in 1..@grid_resolution do
+        @tiles << Tile.new([x, y], [0x0c*x, 0xff, 0x0c*y])
+      end
+    end
+    puts @tiles
+  end
+
+  def draw
+    @tiles.each do |tile|
+      tile_position = tile.pos.collect {|pos| (pos * @scale) }
+      tile_size     = tile.pos.collect {|pos| (pos * @scale) + @scale }
+      @screen.draw_box_s(tile_position, tile_size, tile.color)
+    end
+    player_position = @player.pos.collect {|pos| (pos * @scale) }
+    player_size     = @player.pos.collect {|pos| (pos * @scale) + @scale }
+    @screen.draw_box_s(player_position, player_size, @player.color)
   end
 
   def draw_box
@@ -71,6 +76,22 @@ class Tile
   def initialize(pos, color)
     @pos = pos
     @color = color
+  end
+end
+
+class Player < Tile
+
+  def move(args)
+    case args
+    when :up
+      @pos[1] -= 1
+    when :down
+      @pos[1] += 1
+    when :left
+      @pos[0] -= 1
+    when :right
+      @pos[0] += 1
+    end
   end
 end
 
